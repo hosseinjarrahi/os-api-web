f891207ee8af6c308c88ceb60bd0d35f01343712
Hossein <hj021hj@gmail.com>
2024-08-14 09:56:42 +0330

    a

--- index.py
+++ index.py
@@ -41,11 +41,10 @@ async def runPos(websocket, data):
     # return await websocket.send('0020{"key":"' + data.get('key') + '","cmd":10,"resp":0,"pan":"603770**1285","rrn":"806720040564","terminal":"99028391","trace":"13880","serial":"003193","amount":"11135850","settlement":"11135850","discount":"0","data1":"BK007\u0098\u00d4\u00c7\u00e6\u00d1\u00d2\u00edDT012240727103513RL0011FP0012SP0011T9009236475393IB026IR400590047200404655354002"}')
 
     print('send')
-    
     print(data['amount'])
 
     # res = await pos.send(data['amount'],data['url'])
-    res = await pos.send(12000,data['url'])
+    res = await pos.send(data.get('amount'), data.get('url'))
     
     # todo: should find { and then automaticly detect index of { and then insert key in string
     if 'key' in data:
@@ -60,7 +59,7 @@ def printFile(data):
     pdf_file_path = root_path + "\\print.docx"
     template_file_path = root_path + "\\" + data['template']
     convertToDocx.run(template_file_path, data['context'])
-    printer_name = config.get("PRINTER")
+    printer_name = data.get("printer") if data.get("printer") else config.get("PRINTER")
     paper_size = win32con.DMPAPER_A4  # Example: Set the paper size to A4
     print_pdf(pdf_file_path, printer_name, paper_size,count)
 
